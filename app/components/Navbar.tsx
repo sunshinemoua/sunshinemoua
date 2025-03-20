9e5bc139c33c36cf2ce349bfefa78d8972938554
sunshinemoua <sunshinemoua21@gmail.com>
2025-03-20 15:11:39 -0700

    Refactor Navbar component to remove dark mode functionality and update styles for light mode only. Clean up global CSS by removing dark mode media query.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,13 +3,7 @@ import Link from "next/link";
 import { assets } from "@/assets/assets";
 import { useEffect, useRef, useState } from "react";
 
-const Navbar = ({
-  isDarkMode,
-  setIsDarkMode,
-}: {
-  isDarkMode: boolean;
-  setIsDarkMode: (isDarkMode: boolean) => void;
-}) => {
+const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const sideMenuRef = useRef<HTMLUListElement>(null);
 
@@ -37,7 +31,7 @@ const Navbar = ({
 
   return (
     <>
-      <div className="w-11/12 fixed top-0 right-0 -z-10 translate-y-[-80%] dark:hidden">
+      <div className="w-11/12 fixed top-0 right-0 -z-10 translate-y-[-80%]">
         <Image
           src={assets.header_bg_color}
           alt="header_bg_color"
@@ -47,7 +41,7 @@ const Navbar = ({
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
           isScrolled
-            ? "bg-white bg-opacity-50 background-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20"
+            ? "bg-white bg-opacity-50 background-blur-lg shadow-sm"
             : ""
         }`}
       >
@@ -60,9 +54,7 @@ const Navbar = ({
         </Link>
         <ul
           className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
-            isScrolled
-              ? ""
-              : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"
+            isScrolled ? "" : "bg-white shadow-sm bg-opacity-50"
           }`}
         >
           <li>
@@ -93,48 +85,33 @@ const Navbar = ({
         </ul>
 
         <div className="flex items-center gap-4">
-          <button
-            onClick={() => setIsDarkMode(!isDarkMode)}
-            className="cursor-pointer"
-          >
-            <Image
-              src={isDarkMode ? assets.sun_icon : assets.moon_icon}
-              alt="moon_icon"
-              className="w-6"
-            />
+          <button className="cursor-pointer">
+            <Image src={assets.sun_icon} alt="moon_icon" className="w-6" />
           </button>
           <Link
             href="#contact"
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full dark:border-white/50"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full "
           >
             Contact
-            <Image
-              src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon}
-              alt="contact"
-              className="w-3"
-            />
+            <Image src={assets.arrow_icon} alt="contact" className="w-3" />
           </Link>
           <button
             className="block md:hidden ml-3 cursor-pointer"
             onClick={handleOpenMenu}
           >
-            <Image
-              src={isDarkMode ? assets.menu_white : assets.menu_black}
-              alt="menu_black"
-              className="w-6"
-            />
+            <Image src={assets.menu_black} alt="menu_black" className="w-6" />
           </button>
         </div>
 
         {/* Mobile Menu */}
         <ul
           ref={sideMenuRef}
-          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkTheme dark:text-white"
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
         >
           <div className="absolute right-6 top-6">
             <Image
               onClick={handleCloseMenu}
-              src={isDarkMode ? assets.close_white : assets.close_black}
+              src={assets.close_black}
               alt="close_black"
               className="w-5 cursor-pointer"
             />
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,10 @@
 "use client";
 import Navbar from "./components/Navbar";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-
-  useEffect(() => {
-    if (
-      localStorage.getItem("theme") === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [isDarkMode]);
-
   return (
     <>
-      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Navbar />
     </>
   );
 }
