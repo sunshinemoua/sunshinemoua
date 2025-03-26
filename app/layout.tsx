9482800ade487bfba2c8860ddacfb5ade89b51ca
sunshinemoua <sunshinemoua21@gmail.com>
2025-03-25 18:59:37 -0700

    enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { DM_Serif_Text } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const dmSerifText = DM_Serif_Text({
   weight: "400",
@@ -42,6 +43,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
